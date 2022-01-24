680de899ab66c29c888b4f3e92aea1d00afa9a73
karpova.m <34@3389.ru>
2022-01-24 17:39:45 +0700

    Ссылка на базу fire base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ import {Environment} from "./interface";
 export const environment: Environment = {
   production: true,
   apiKey: 'AIzaSyDNAOwe9RJNx4AR2brY_oLIgM8MS3ktZJc',
+  fbDbUrl: 'https://angularblogtest-1b8f2-default-rtdb.firebaseio.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ import {Environment} from "./interface";
 export const environment: Environment = {
   production: false,
   apiKey: 'AIzaSyDNAOwe9RJNx4AR2brY_oLIgM8MS3ktZJc',
+  fbDbUrl: 'https://angularblogtest-1b8f2-default-rtdb.firebaseio.com/',
 };
 
 /*
