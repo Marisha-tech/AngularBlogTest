6be62746202eeb80c47ca01e0280386352105c8d
karpova.m <34@3389.ru>
2022-01-24 17:30:22 +0700

    Проверка на авторизацию пользователя

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -13,14 +13,24 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup | any
   submitted = false
+  message: string = ''
 
   constructor(
-    private auth: AuthService,
-    private router: Router//для редиректа
+    public auth: AuthService, //публичный, т.к. будет использован в шаблоне
+    private router: Router,//для редиректа
+    private route: ActivatedRoute, // текущий роут
   ) {
   }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params['loginAgain']) {
+        this.message = 'Пожалуйста, авторизуйтесь'
+      } else if (params['authFaled']) {
+        this.message = 'Сессия истекла. Авторизуйтесь'
+      }
+    })
+
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
@@ -45,6 +55,8 @@ export class LoginPageComponent implements OnInit {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])//редирект на страницу dashboard
       this.submitted = false
+    }, () => {
+      this.submitted = false // если есть ошибка, тогда разблокировать кнопку
     })
 
   }
