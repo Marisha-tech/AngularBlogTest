2165f2d1d0755fb14ae0946afe06f04eac10663c
karpova.m <34@3389.ru>
2022-01-24 17:36:22 +0700

    Создание перехватчика для проверки авторизации

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,46 @@
+import {Injectable} from "@angular/core";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {AuthService} from "../admin/shared/services/auth.service";
+import {Router} from "@angular/router";
+import {catchError, tap} from "rxjs/operators";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.auth.isAuthentificated()) {//если авторизован, то клонируем req и в параметр записываем токен
+      req = req.clone({
+        setParams: {
+          auth: this.auth.token
+        }
+      })
+    }
+    return next.handle(req)
+
+      .pipe(
+        tap(() => {
+          console.log('Intercept')
+        }),
+        catchError((error: HttpErrorResponse) => {
+          console.log('Interceptor error', error)
+          if (error.status === 401) {//если статус ошибки 401, то вызвать метод logout
+            this.auth.logout()
+            //переадресация на страницу login
+            this.router.navigate(['/admin', 'login'], {
+              queryParams: {
+                authFailed: true
+              }
+            })
+          }
+          return throwError(error) //throwError импортируется из rxjs
+        })
+      )
+  }
+
+}
