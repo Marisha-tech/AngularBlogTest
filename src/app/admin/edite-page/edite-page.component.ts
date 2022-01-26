0885897e53c293982d9a2e888a5e7662b84515bb
karpova.m <34@3389.ru>
2022-01-26 11:42:10 +0700

    Alert на обновление поста

--- src/app/admin/edite-page/edite-page.component.ts
+++ src/app/admin/edite-page/edite-page.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {PostsService} from "../../shared/posts.service";
 import {switchMap} from "rxjs/operators";
 import {Post} from "../../shared/interfaces";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
+import {AlertService} from "../shared/services/alert.service";
 
 @Component({
   selector: 'app-edite-page',
   templateUrl: './edite-page.component.html',
   styleUrls: ['./edite-page.component.scss']
 })
-export class EditePageComponent implements OnInit {
+export class EditePageComponent implements OnInit, OnDestroy {
 
   form: FormGroup | any
   post: Post | any //для хранения id поста
   submitted = false //флаг для обновления формы
 
+  uSub: Subscription | any
+
   constructor(
     private route: ActivatedRoute,
-    private postService: PostsService
+    private postService: PostsService,
+    private alert: AlertService
   ) { }
 
   ngOnInit(): void {
@@ -30,7 +35,7 @@ export class EditePageComponent implements OnInit {
         switchMap((params: Params) => {
           return this.postService.getById(params['id'])
         })).subscribe((post: Post) => {
-          this.post = post
+          this.post = post //для хранения id поста
           this.form = new FormGroup({
             title: new FormControl(post.title, Validators.required),
             text: new FormControl(post.text, Validators.required),
@@ -42,19 +47,27 @@ export class EditePageComponent implements OnInit {
       //после того, как получили params[id] его нужно передать в новый стрим
   }
 
+  ngOnDestroy() {
+    if (this.uSub){
+      this.uSub.unsubscribe()
+    }
+  }
+
   submit() {
+    //если форма не валидна, то не отправлять форму
     if (this.form.invalid) {
       return
     }
 
     this.submitted = true
-
-    this.postService.update({
-      ...this.post,
+    //обновление поста
+    this.uSub = this.postService.update({
+      ...this.post,//для хранения инфы поста
       title: this.form.value.title,
       text: this.form.value.text,
     }).subscribe(() => {
       this.submitted = false
+      this.alert.warning('Пост обновлен')
     })
   }
 }
