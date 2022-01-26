3d7274fcc1a4a8786679950725d696516f68564e
karpova.m <34@3389.ru>
2022-01-26 16:59:36 +0700

    Добавлен ServiceWorkerModule для работы с приложением

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {registerLocaleData} from "@angular/common";
 import ruLocale from "@angular/common/locales/ru";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 import {AppRoutingModule} from './app-routing.module';
@@ -13,6 +14,7 @@ import {PostPageComponent} from './post-page/post-page.component';
 import {PostComponent} from './shared/components/post/post.component';
 import {SharedModule} from "./shared/shared.module";
 import {AuthInterceptor} from "./shared/auth.interceptor";
+import { environment } from '../environments/environment';
 
 //локализация для вывода даты на русском
 registerLocaleData(ruLocale, 'ru')
@@ -35,6 +37,12 @@ const INTERCEPTOR_PROVIDER: Provider = {
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
