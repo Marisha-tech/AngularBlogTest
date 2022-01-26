4fba05149d7144000cdb433b14f6a17b24941c24
karpova.m <34@3389.ru>
2022-01-26 14:41:31 +0700

    Вывод информации поста на странице поста

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostsService} from "../shared/posts.service";
+import {Observable} from "rxjs";
+import {Post} from "../shared/interfaces";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  //TypeScript включает строгую проверку классов, где все свойства должны быть инициализированы в конструкторе. Обходной путь — добавить !постфикс к имени переменной
+  post$!: Observable<Post>
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.post$ = this.route.params
+      //изменить направление стримов от params до нужного стрима
+      .pipe(switchMap((params: Params) => {
+        return this.postsService.getById(params['id'])
+      }))
   }
 
 }
